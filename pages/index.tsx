ff9ed30fbd37db6de7bf5abe1488275b20e3dfeb
unknown <matheusbprossi@gmail.com>
2022-11-05 19:55:53 -0300

    connected to server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+interface HomeProps{
+  count: number
+}
+
+export default function Home(props:HomeProps) {
   return (
-    <h1>Hello World</h1>
+    <h1>contagem: {props.count}</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  return{
+    props: {
+      count: data.count,
+    }
+  }
 }
